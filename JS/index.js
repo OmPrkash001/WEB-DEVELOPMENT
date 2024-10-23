c43b0b4b02fd579546732158666ac527d9b51300
OmPrkash001 <omprakashupadhyay886@gmail.com>
2024-10-23 11:36:12 +0530

    web development

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,40 @@
-var a;
-var a;
+// var a;
+// var a;
 
-// let b;
-// let b;
+// // let b;
+// // let b;
 
-// const c;
-const d = 1;
+// // const c;
+// const d = 1;
 
-const cc = 10;
-cc = "10"
+// const cc = 10;
+// cc = "10"
 
-{
-    let ab = 10;
-    let cb = ab+20;
-}
+// {
+//     let ab = 10;
+//     let cb = ab+20;
+// }
 
-let ddd = ab+20;
+// let ddd = ab+20;
 
-console.log(ddd);
+// console.log(ddd);
+
+// Math Object
+// let a = 10.5;
+// console.log(Math.round(a));
+// console.log(Math.floor(a));
+// console.log(Math.ceil(a));
+
+console.log(Math.random());
+
+console.log(Math.random()*10);
+
+let randomVal = Math.random()*10;
+console.log(Math.round(randomVal));
+
+console.log(Math.round(Math.round(Math.random()*10)));
+
+// Random in range 5 to 10
+//console.log(Math.round(Math.random()*(max-min)+min));
+
+console.log(Math.round(Math.random()*(10-5))+5);
