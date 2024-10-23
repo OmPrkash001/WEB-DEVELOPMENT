1116c4091ccdcf8195e139f93da5c84e206ac624
OmPrkash001 <omprakashupadhyay886@gmail.com>
2024-10-23 11:16:48 +0530

    web development

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+var a;
+var a;
+
+// let b;
+// let b;
+
+// const c;
+const d = 1;
+
+const cc = 10;
+cc = "10"
+
+{
+    let ab = 10;
+    let cb = ab+20;
+}
+
+let ddd = ab+20;
+
+console.log(ddd);
